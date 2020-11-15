0580e41198772c09fbd3153cfd20072b9b2a465b
maksym-gryb <m.gryb09@gmail.com>
2020-11-14 20:52:27 -0800

    InfoMessage class completed

--- BlockMeInTime/InfoMessage.cs
+++ BlockMeInTime/InfoMessage.cs
@@ -4,49 +4,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace BlockMeInTime
 {
-    class InfoMessage : WrapPanel
+    class InfoMessage : TextBlock
     {
-        private TextBlock text;
         private System.Windows.Threading.DispatcherTimer fadeDispatcherTimer = new System.Windows.Threading.DispatcherTimer();
         private System.Windows.Threading.DispatcherTimer fadingDispatcherTimer = new System.Windows.Threading.DispatcherTimer();
 
-        private float fade_duration_interval;
-        InfoMessage() : base()
+        private Color foreground_color = Colors.Red;
+
+        private Color ForegroundColor
         {
-            this.AddVisualChild(text);
+            get
+            {
+                return foreground_color;
+            }
+
+            set
+            {
+                foreground_color = value;
+                Foreground = new SolidColorBrush(foreground_color);
+            }
         }
 
-        /*
-
-        public void PopUp(int time_to_fade, int fade_duration, string message)
+        public InfoMessage() : base()
         {
-            text.Text = message;
+            Background = Brushes.Black;
+            Foreground = Brushes.Red;
+
+            Text = "";
+        }
+
+        public void Show(int time_to_fade, int fade_duration, string message)
+        {
+            Text = message;
 
             fadeDispatcherTimer.Interval = new TimeSpan(0, 0, 0, time_to_fade, 0);
             fadeDispatcherTimer.Tick += Fade;
             fadeDispatcherTimer.Start();
 
-            fadingDispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, (int)(1000 * fade_duration / 20));
-
-            base.Show();
+            fadingDispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, (int)(1000 * fade_duration / 100));
+            fadingDispatcherTimer.Tick += Fading;
         }
+
 
         private void Fade(object sender, EventArgs e)
         {
+            fadeDispatcherTimer.Stop();
             fadingDispatcherTimer.Start();
         }
 
         private void Fading(object sender, EventArgs e)
         {
-            fadeDispatcherTimer.Stop();
-            this.Background.alpha; //get alpha
+            Color faded_color = new Color();
 
-            if (alpha < 20) -> base.Hide(); && fadingDispatcherTimer.Stop();
+            byte fade_amount = 20;
+            faded_color.A = foreground_color.A;
+            faded_color.A -= fade_amount;
+            faded_color.R = foreground_color.R;
+            faded_color.G = foreground_color.G;
+            faded_color.B = foreground_color.B;
+
+            if (faded_color.A < fade_amount)
+            {
+                faded_color.A = 0;
+                fadingDispatcherTimer.Stop();
+            }
+
+            ForegroundColor = faded_color;
         }
-
-        */
     }
 }
